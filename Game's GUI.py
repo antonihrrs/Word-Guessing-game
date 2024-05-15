e1fc46109ac604d67b284114d15d1ba2be283421
Antoni <159946052+antonihrrs@users.noreply.github.com>
2024-05-15 13:33:50 +0200

    Update Game's GUI.py
    + correcting grammar error

--- Game's GUI.py
+++ Game's GUI.py
@@ -6,7 +6,7 @@ window = tkinter.Tk()
 window.title("🔮 Guess them all 🔮")
 window.geometry("500x300")
 
-words_list = ["infrastructure","compactness", "topology", "transcendence", "divination", "ommniscient", "effervescent", 
+words_list = ["infrastructure","compactness", "topology", "transcendence", "divination", "omniscient", "effervescent", 
              "echolocation", "vicissitudes", "homeomorphism", "multifariousness", "acupuncture", "malevolent", "quintessential", "promiscuous",
               "ephemeral", "luminious", "cacophony", "rehabilitation", "testimony", "encryption", "jurisdiction", "prescription", "thermodynamic",
                "accreditation", "biodegradable", "sustainability", "segmentation", "psychopathology", "aesthetic", "correspondent", "cultivation",
@@ -116,19 +116,91 @@ def blank_spaces():
         letters_by_letters.config(text=letters_by_letters.cget("text") + " _ ")
     letters_by_letters.pack()
 
-def user_guess_box():
-    user_box_guess = tkinter.Entry(window)
-    user_box_guess.pack()
-
 tries = 0
 def tries_nb():
     global tries
     number_of_tries.config(text= f"Tries = {tries}")
 
+def check():
+    global tries
+    global guessed_word
+    global display_guessed_word
+
+    user_letter = user_box_guess.get()
+    check_button.pack()
+    if "_" in guessed_word:
+
+        if user_letter.isalpha()and len(user_letter) == 1 :
+            tries += 1
+            tries_nb()
+            if user_letter in word :
+                for i in range(len(word)):
+                    if word[i] == user_letter:
+                        guessed_word[i] = user_letter
+                display_guessed_word = " ".join(guessed_word)
+                letters_by_letters.config(text=display_guessed_word)
+                user_box_guess.delete(0, 'end')
+                
+            else:
+                letter_not_in_word.config(text=f"' {user_letter} ' is not in the word")
+                letter_not_in_word.pack()
+                user_box_guess.delete(0, 'end')
+                window.after(1000, lambda: letter_not_in_word.forget())
+        
+        else:
+            nope_message.config(text="🪄 Nope")
+            nope_message.pack()
+            user_box_guess.delete(0, 'end')
+            window.after(1000, lambda: nope_message.forget())
+    
+    else:
+        word_lengh_text.forget()
+        check_button.forget()
+        user_box_guess.forget()
+        letters_by_letters.forget()
+        end_message.config(text=f"Congrats {user_name} ✨ \nYou've guessed the word '{word}' correctly !")
+        end_message.pack()
+        window.after(2000, lambda: happy_end_message())
+
+def happy_end_message():
+
+    if tries >= len(word) * 2.5:
+        number_of_tries_message.config(text=f"Ouch... It took you {tries} tries to guess {word_length} letters !")
+        number_of_tries_message.pack()
+            
+    elif tries <= len(word) * 2.5 and tries >= len(word) * 1.75:
+        number_of_tries_message.config(text=f"It took you {tries} tries to find those {word_length} letters.")
+        number_of_tries_message.pack()
+
+    elif tries <= len(word) * 1.75 and tries >= len(word) * 1.1:
+        number_of_tries_message.config(text=f"It only took you {tries} tries to guess all {word_length} letters ! Nice")
+        number_of_tries_message.pack()
+    else:
+        number_of_tries_message.config(text=f"Woah it only took you {tries} tries to guess '{word}'\nand all it's {word_length} letters !\n🤯 Amazing !")
+        number_of_tries_message.pack()
+    
+    window.after(2500, lambda:play_again_ask())
+
+def play_again_ask():
+    play_again_message.config(text="Play again ?")
+    play_again_message.pack()
+    window.after(1200, lambda: play_again_button.pack())
+    window.after(1400, lambda:leave_button.pack())
+
+def play_again_clicked():
+    play_again_button.forget()
+    end_message.forget()
+    number_of_tries_message.forget()
+    play_again_button.forget()
+    play_again_message.forget()
+    letters_gen_showing_up.pack()
+    begin()
+  
 def game():
     word_text.pack_forget()
     window.after(500, lambda: blank_spaces())
     window.after(500, lambda: user_guess_box())
+    check_button.pack()
 
 user_box = tkinter.Entry(window)
 user_box.place(anchor="center")
@@ -144,6 +216,8 @@ yes_button = tkinter.Button(window, text="Yes", command=conclude_get_name)
 no_button = tkinter.Button(window, text="No", command=ask_name_again)
 start_button = tkinter.Button(window, text="Start", command=begin)
 leave_button = tkinter.Button(window, text="Leave", command=leaving)
+check_button = tkinter.Button(window, text="Check", anchor="s", command=check)
+play_again_button = tkinter.Button(window, text="Pay again", command=play_again_clicked)
 
 letters_gen = tkinter.StringVar()
 letters_gen_showing_up = tkinter.Label(window, textvariable=letters_gen)
@@ -166,6 +240,9 @@ guessed_word = tkinter.Label(window)
 character_number = tkinter.Label(window)
 letters_by_letters = tkinter.Label(window)
 word_lengh_text = tkinter.Label(window)
-
+letter_not_in_word = tkinter.Label(window)
+nope_message = tkinter.Label(window)
+end_message =tkinter.Label(window)
+play_again_message = tkinter.Label(window)
 
 window.mainloop()
